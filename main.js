87a7bc485d54af79dd645a89cded19d3d24b7580
TakujiMoyama <takuji.moyama@avanade.com>
2024-05-08 12:13:03 +0900

    算出プロパティを作ろう
    getとset

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const app = Vue.createApp({
     data: () => ({
-        url: 'https://www.google.co.jp/'
-    })
+        basePrice: 100
+    }),
+    computed:{
+        taxIncludedPrice:{
+            get: function(){
+                return this.basePrice *1.1
+            },
+            set: function(value){
+                this.basePrice = value / 1.1
+            }
+        }
+    }
 })
 app.mount('#app')
