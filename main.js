8a7aa141a40163cc08272044787a006a93ed945f
TakujiMoyama <takuji.moyama@avanade.com>
2024-05-09 10:24:58 +0900

    QiitaAPIを使って記事の検索

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
-// const app = Vue.createApp({
-//     data: () => ({
-//         firstName: "",
-//         lastName: "",
-//         fullName: ""
-//     }),
-//     watch:{
-//         firstName: function(value){
-//             this.fullName = value + ' ' + this.lastName
-//         },
-//         lastName: function(value){
-//             this.fullName = this.firstName + ' ' + value
-//         }
-//     }
-// })
 const app = Vue.createApp({
     data: () => ({
-        firstName: "",
-        lastName: "",
+        items: null,
+        keyword: '',
+        message: ''
     }),
-    computed: {
-        fullName: function(){
-            return this.firstName + ' ' + this.lastName
+    watch: {
+        keyword: function(newkeyword, oldkeyword){
+            console.log(newkeyword)
+            this.message = 'Waiting for you to stop typing'
+            this.debouncedGetAnswer()
+        }
+
+    },
+    mounted: function(){
+        // this.keyword = 'JavaScript'
+        // this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
+    methods: {
+        getAnswer: function() {
+            if(this.keyword === '') {
+                console.log('karamoji')
+                this.items = null
+                return
+            }
+
+            this.message = 'Loading...'
+            const vm = this
+            const params = { page: 1, per_page: 20, query: this.keyword}
+            axios.get('http://qiita.com/api/v2/items',{ params })
+                .then(function(response){
+                    // console.log(response)
+                    vm.items = response.data
+                })
+                .catch(function(error){
+                    vm.message = 'Error!' + error
+                })
+                .finally(function(){
+                    vm.message = ''
+                })
         }
     }
 })
